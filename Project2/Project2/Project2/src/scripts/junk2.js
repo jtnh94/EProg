cd06253e955aab054a6ef259c66d11f001398be8
Jonathan Hoang <jhoang2@uco.edu>
2019-02-26 16:48:37 -0600

    Project 2 Complete

--- Project2/Project2/Project2/src/scripts/junk2.js
+++ Project2/Project2/Project2/src/scripts/junk2.js
@@ -1,9 +1,13 @@
 ﻿import '../styles/bg1.css';
 import pic from '../images/pic.jpg';
-import pic2 from '../images/game.jpg';
-import pic3 from '../images/blog.jpg';
 
 (function () {
     var selfPic = document.getElementById('picture');
-    selfPic.src = pic;
+
+    var image = new Image();
+    image.src = pic;
+
+    selfPic.appendChild(image);
+    
+
 })();
